20a808ba408280ac8bfe1094f82bc6d588318a4f
Douglas Engratulis <dengratulis@gmail.com>
2024-11-15 14:12:38 -0300

    Treinando lógica de coleções HashSet e SortedSet

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Training_SortedSet_and_Hashset {
+    internal class Program {
+        static void Main(string[] args) {
+
+            HashSet<int> hash = new HashSet<int>();
+
+            string[] Vetcourses = { "A", "B", "C" };
+
+            for (int i = 0; i < Vetcourses.Length; i++) {
+                Console.Write("How many students on Course " + Vetcourses[i] + "?");
+                int n = int.Parse(Console.ReadLine());
+                for (int j = 0; j < n; j++) {
+                    hash.Add(int.Parse(Console.ReadLine()));
+                }
+            }
+            int quantity = hash.Count;
+            Console.WriteLine("Total students: " + quantity);
+
+        }
+    }
+}
